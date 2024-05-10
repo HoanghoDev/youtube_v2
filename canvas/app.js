3afb501ba71e265811bbf56c3badecbaaf9f9dc6
h_ho <hohoang.dev@gmail.com>
2024-05-10 17:17:05 +0700

    canvas

--- canvas/app.js
+++ canvas/app.js
@@ -1,55 +1,64 @@
 let banner = document.querySelector('.banner');
+let canvas = document.getElementById('dotsCanvas');
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+let ctx = canvas.getContext('2d');
 
-    const canvas = document.getElementById('dotCanvas');
-    const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    const dots = [];
-    const maxDots = 50; 
-
-    const arrayColors = ['#eee', '#545454', '#596d91', '#bb5a68', '#696541'];
-
-    for (let i = 0; i < maxDots; i++) {
-        const color = arrayColors[Math.floor(Math.random() * 5)];
-
-        const x = Math.floor(Math.random() * canvas.width);
-        const y = Math.floor(Math.random() * canvas.height);
-
-        const size = Math.random() * 3 + 5;
-        dots.push({ x, y, color, size });
+let dots = [];
+let arrayColors = ['#eee', '#545454', '#596d91', '#bb5a68', '#696541'];
+for (let index = 0; index < 50; index++) {
+    dots.push({
+        x:  Math.floor(Math.random() * canvas.width),
+        y:  Math.floor(Math.random() * canvas.height),
+        size: Math.random() * 3 + 5,
+        color: arrayColors[Math.floor(Math.random()* 5)]
+    });
+}
+const drawDots = () => {
+    dots.forEach(dot => {
+        ctx.fillStyle = dot.color;
+        ctx.beginPath();
+        ctx.arc(dot.x, dot.y, dot.size, 0, Math.PI*2);
+        ctx.fill();
+    })
+}
+drawDots();
+banner.addEventListener('mousemove', (event) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawDots();
+    let mouse = {
+        x:  event.pageX - banner.getBoundingClientRect().left,
+        y:  event.pageY - banner.getBoundingClientRect().top
     }
-
-    function draw() {
-        dots.forEach((dot, index) => {
-            ctx.fillStyle = dot.color;
+    dots.forEach(dot => {
+        let distance = Math.sqrt((mouse.x - dot.x) ** 2 + (mouse.y - dot.y) ** 2);
+        if(distance < 300){
+            ctx.strokeStyle = dot.color;
+            ctx.lineWidth = 1;
             ctx.beginPath();
-            ctx.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
-            ctx.fill();
+            ctx.moveTo(dot.x, dot.y);
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+        }
+    })
+})
+banner.addEventListener('mouseout', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawDots();
+})
+window.addEventListener('resize', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = banner.offsetWidth;
+    canvas.height = banner.offsetHeight;
+
+    dots = [];
+    for (let index = 0; index < 50; index++) {
+        dots.push({
+            x:  Math.floor(Math.random() * canvas.width),
+            y:  Math.floor(Math.random() * canvas.height),
+            size: Math.random() * 3 + 5,
+            color: arrayColors[Math.floor(Math.random()* 5)]
         });
     }
-
-    draw();
-    banner.addEventListener('mousemove', (event) => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        draw();
-        const mouse = {
-            x: event.clientX,
-            y: event.clientY
-        }
-        dots.forEach((dot, index) => {
-            const distance = Math.sqrt((mouse.x - dot.x) ** 2 + (mouse.y - dot.y) ** 2);
-            if (distance < 300) {
-                ctx.strokeStyle = dot.color;
-                ctx.lineWidth = 1;
-                ctx.beginPath();
-                ctx.moveTo(dot.x, dot.y);
-                ctx.lineTo(mouse.x, mouse.y);
-                ctx.stroke();
-            }
-        })
-})
-banner.addEventListener('mouseout', (event) => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    draw();
+    drawDots();
 })
